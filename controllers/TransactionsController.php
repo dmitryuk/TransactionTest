15812e6327983136c8ea9e09a34c66e9a2b8baec
Alex <iprodex@mail.ru>
2016-09-07 08:49:00 +0700

    asd

--- controllers/TransactionsController.php
+++ controllers/TransactionsController.php
@@ -1,28 +1,1 @@
-<?php
-
-namespace app\controllers;
-
-use app\models\LoginForm;
-use app\models\TransactionForm;
-use app\models\Users;
-
-class TransactionsController extends \yii\web\Controller
-{
-
-
-    public function actionIndex()
-    {
-        $users = Users::find()->all();
-        $model = new TransactionForm();
-        if ($model->load(\yii::$app->request->post()) && $model->Validate() && $model->makeTransaction()) {
-            return $this->redirect(array('success'));
-        }
-        return $this->render('main', ['users' => $users, 'model' => $model]);
-    }
-
-    public function actionSuccess()
-    {
-        return $this->render('success');
-    }
-
-}
+<?phpnamespace app\controllers;use app\models\LoginForm;use app\models\TransactionForm;use app\models\Users;class TransactionsController extends \yii\web\Controller{    public function actionIndex()    {        $users = Users::find()->all();        $model = new TransactionForm();        if ($model->load(\yii::$app->request->post()) && $model->Validate() && $model->makeTransaction()) {            return $this->redirect(array('success'));        }        return $this->render('main', ['users' => $users, 'model' => $model]);    }    public function actionSuccess()    {        return $this->render('success');    }}
